2fbf2590813b5f3542d015671ea50fcadb483cfa
Flávio Bulhões <flavio.profissional1@gmail.com>
2022-09-14 08:28:35 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/main.css'
+import logoImg from '/logo.svg'
+
+
+function App() {
+  return (
+    
+  <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+    <img src={logoImg} />
+    <h1 className='text-6xl text-white font-black mt-20'>Seu duo está aqui</h1>
+  </div>
+  
+  )
+}
+
+export default App
+
